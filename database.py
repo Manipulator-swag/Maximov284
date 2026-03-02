8e12153b13e43337c0bd8d2d6bd6a0da9032696c
Swag Master <bgdan2006raf@gmail.com>
2026-03-02 21:36:55 +0500

    Create DataBase connection

---
+++ database.py
@@ -0,0 +1,18 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:password@localhost/testdb")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
